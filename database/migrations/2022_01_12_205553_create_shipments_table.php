4fbc9666690423c6da7ab6e6aa6cc20756b2f682
omarchouman <omar.chouman0@gmail.com>
2022-01-12 23:24:01 +0200

    Finished Migrations

---
+++ database/migrations/2022_01_12_205553_create_shipments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->string('customer_city');
+            $table->string('customer_country');
+            $table->string('customer_phone');
+            $table->string('customer_details');
+            $table->integer('user_id');
+            $table->integer('address_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
--- database/migrations/2022_01_12_212056_create_shipment_products_table.php
+++ database/migrations/2022_01_12_212056_create_shipment_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateShipmentProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shipment_products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone');
-            $table->string('directive')->nullable();
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('shipment_id');
+            $table->integer('product_id');
+            $table->integer('product_qty');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shipment_products');
     }
 }
