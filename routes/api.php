f156033b19519a1238144e01526f6c0fd5c26435
omarchouman <omar.chouman0@gmail.com>
2022-01-13 20:29:52 +0200

    Finished JWT Authentication With Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Authentication Routes Here
+Route::post('/register', ['App\Http\Controllers\AuthController', 'register']);
+Route::post('/login', ['App\Http\Controllers\AuthController', 'login']);
+Route::post('/logout', ['App\Http\Controllers\AuthController', 'logout']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
+Route::get('/user-profile', [AuthController::class, 'userProfile']);
